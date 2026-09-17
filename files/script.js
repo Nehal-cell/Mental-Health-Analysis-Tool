3182cbff79170e5e8c1a364486c48327a552d928
Nehal <nehal@Nehals-MacBook-Air.local>
2026-09-17 21:18:15 +0530

    Updated Js file with deployed backend link

--- files/script.js
+++ files/script.js
@@ -1,4 +1,4 @@
-const API_BASE = "http://127.0.0.1:8000";
+const API_BASE = "https://mental-health-analysis-tool.onrender.com/";
 
 const form = document.getElementById('predict-form');
 const submitBtn = document.getElementById('submit-btn');
